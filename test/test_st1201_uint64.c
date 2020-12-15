d66ea46dc3209941ca3e73ab7c36cdddc4b0512a
Danilo Horta <danilo.horta@pm.me>
2020-12-15 18:48:13 +0000

    avoid redundant test

--- test/test_st1201_uint64.c
+++ test/test_st1201_uint64.c
@@ -5,9 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void test_st1201_uint8(struct line line);
-static void test_st1201_uint16(struct line line);
-static void test_st1201_uint32(struct line line);
 static void test_st1201_uint64(struct line line);
 
 int main(void)
@@ -25,57 +22,12 @@ int main(void)
         nrow++;
 
         struct line line = parse_line(row);
-        if (line.L == 1)
-            test_st1201_uint8(line);
-        else if (line.L == 2)
-            test_st1201_uint16(line);
-        else if (line.L == 4)
-            test_st1201_uint32(line);
-        else if (line.L == 8)
+        if (line.L == 8)
             test_st1201_uint64(line);
     }
 
     fclose(stream);
     return cass_status();
-}
-
-static void test_st1201_uint8(struct line line)
-{
-    struct st1201_uint8 st1201;
-    cass_cond(st1201_init_uint8(line.a, line.b, &st1201) == 0);
-    cass_close(st1201.impl.a, line.a);
-    cass_close(st1201.impl.b, line.b);
-    cass_close(st1201.impl.S_f, line.S_f);
-    cass_close(st1201.impl.S_r, line.S_r);
-    cass_close(st1201.impl.Z_o, line.Z_o);
-    cass_equal_uint8(st1201_to_uint8(&st1201, line.x), line.y);
-    cass_close(st1201_from_uint8(&st1201, line.y), line.x_r);
-}
-
-static void test_st1201_uint16(struct line line)
-{
-    struct st1201_uint16 st1201;
-    cass_cond(st1201_init_uint16(line.a, line.b, &st1201) == 0);
-    cass_close(st1201.impl.a, line.a);
-    cass_close(st1201.impl.b, line.b);
-    cass_close(st1201.impl.S_f, line.S_f);
-    cass_close(st1201.impl.S_r, line.S_r);
-    cass_close(st1201.impl.Z_o, line.Z_o);
-    cass_equal_uint16(st1201_to_uint16(&st1201, line.x), line.y);
-    cass_close(st1201_from_uint16(&st1201, line.y), line.x_r);
-}
-
-static void test_st1201_uint32(struct line line)
-{
-    struct st1201_uint32 st1201;
-    cass_cond(st1201_init_uint32(line.a, line.b, &st1201) == 0);
-    cass_close(st1201.impl.a, line.a);
-    cass_close(st1201.impl.b, line.b);
-    cass_close(st1201.impl.S_f, line.S_f);
-    cass_close(st1201.impl.S_r, line.S_r);
-    cass_close(st1201.impl.Z_o, line.Z_o);
-    cass_equal_uint32(st1201_to_uint32(&st1201, line.x), line.y);
-    cass_close(st1201_from_uint32(&st1201, line.y), line.x_r);
 }
 
 static void test_st1201_uint64(struct line line)
--- test/test_st1201_uint8.c
+++ test/test_st1201_uint8.c
@@ -6,9 +6,6 @@
 #include <stdlib.h>
 
 static void test_st1201_uint8(struct line line);
-static void test_st1201_uint16(struct line line);
-static void test_st1201_uint32(struct line line);
-static void test_st1201_uint64(struct line line);
 
 int main(void)
 {
@@ -27,12 +24,6 @@ int main(void)
         struct line line = parse_line(row);
         if (line.L == 1)
             test_st1201_uint8(line);
-        else if (line.L == 2)
-            test_st1201_uint16(line);
-        else if (line.L == 4)
-            test_st1201_uint32(line);
-        else if (line.L == 8)
-            test_st1201_uint64(line);
     }
 
     fclose(stream);
@@ -50,43 +41,4 @@ static void test_st1201_uint8(struct line line)
     cass_close(st1201.impl.Z_o, line.Z_o);
     cass_equal_uint8(st1201_to_uint8(&st1201, line.x), line.y);
     cass_close(st1201_from_uint8(&st1201, line.y), line.x_r);
-}
-
-static void test_st1201_uint16(struct line line)
-{
-    struct st1201_uint16 st1201;
-    cass_cond(st1201_init_uint16(line.a, line.b, &st1201) == 0);
-    cass_close(st1201.impl.a, line.a);
-    cass_close(st1201.impl.b, line.b);
-    cass_close(st1201.impl.S_f, line.S_f);
-    cass_close(st1201.impl.S_r, line.S_r);
-    cass_close(st1201.impl.Z_o, line.Z_o);
-    cass_equal_uint16(st1201_to_uint16(&st1201, line.x), line.y);
-    cass_close(st1201_from_uint16(&st1201, line.y), line.x_r);
-}
-
-static void test_st1201_uint32(struct line line)
-{
-    struct st1201_uint32 st1201;
-    cass_cond(st1201_init_uint32(line.a, line.b, &st1201) == 0);
-    cass_close(st1201.impl.a, line.a);
-    cass_close(st1201.impl.b, line.b);
-    cass_close(st1201.impl.S_f, line.S_f);
-    cass_close(st1201.impl.S_r, line.S_r);
-    cass_close(st1201.impl.Z_o, line.Z_o);
-    cass_equal_uint32(st1201_to_uint32(&st1201, line.x), line.y);
-    cass_close(st1201_from_uint32(&st1201, line.y), line.x_r);
-}
-
-static void test_st1201_uint64(struct line line)
-{
-    struct st1201_uint64 st1201;
-    cass_cond(st1201_init_uint64(line.a, line.b, &st1201) == 0);
-    cass_close(st1201.impl.a, line.a);
-    cass_close(st1201.impl.b, line.b);
-    cass_close(st1201.impl.S_f, line.S_f);
-    cass_close(st1201.impl.S_r, line.S_r);
-    cass_close(st1201.impl.Z_o, line.Z_o);
-    cass_equal_uint64(st1201_to_uint64(&st1201, line.x), line.y);
-    cass_close(st1201_from_uint64(&st1201, line.y), line.x_r);
 }
